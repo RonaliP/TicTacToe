99b41d1b587e767c22d07bc23a6435ea96cfd209
Ronali <ronalipanigrahy88@gmail.com>
2020-11-10 16:32:55 +0530

    CODE FOR TAKING USER INPUT ADDED

--- TicTac.java
+++ TicTac.java
@@ -1,22 +1,48 @@
 import java.util.*;
 
-class TicTac{
+class TicTac
+{
+      char[] board=new char[9];
+      Scanner sc=new Scanner(System.in);
 
        public static void main(String args[])
        {
-
-	char[] board=CreateBoard();
-
-
+	TicTac t=new TicTac();
+	char[] CreateBoard=t.CreateBoard();
+	t.CheckInput();
        }
 
 
-	public static void CreateBoard()
+	public char[] CreateBoard()
 	{
-         	char[] board=new char[9];
+
 		for(int a=0;a<board.length;a++)
 		{
 			board[a]=' ';
 		}
+		return board;
+	}
+
+	public void  CheckInput()
+	{
+		System.out.println("USER INPUT A CHARACTER \n 1.'x' 2.'o' ");
+		char choice=sc.next().toUpperCase().charAt(0);
+		switch(choice)
+		{
+		case 'X':
+	        	System.out.println("User chose x so opponent will take o");
+		        char User='X';
+			char Opponent='O';
+			break;
+		case 'O':
+			System.out.println("User chose o so opponent will take x");
+			User='O';
+			Opponent='X';
+			break;
+		default:
+			System.out.println("WRONG INPUT,TRY AGAIN");
+			CheckInput();
+		}
+
 	}
 }
